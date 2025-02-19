1768e5c827a638bc4086a3155f274dff2bfe0db6
Sarah Fulkerson <contact@sarahfulkerson.com>
2025-02-19 13:55:21 -0600

    exercises for Pset0 completed
    finished all exercises in Pset0

---
+++ Pset0/ps0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 13:14:17 2025
+
+@author: sarahfulkerson
+"""
+import numpy
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+print("x**y = ", x**y)
+print("log(x) = ", numpy.log2(x))
---
+++ Pset0/pyTest.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 12:52:42 2025
+
+@author: sarahfulkerson
+"""
+
+print("Hello world!")
+print 'I like 6.001!'
