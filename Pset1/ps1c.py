acc2c9d9eb73988a3578f227168fb6784f6ae5a8
Sarah Fulkerson <contact@sarahfulkerson.com>
2025-02-22 14:38:51 -0600

    finally solved ps1c

--- Pset1/ps1c.py
+++ Pset1/ps1c.py
@@ -8,25 +8,43 @@ Created on Fri Feb 21 20:45:28 2025
 
 semi_annual_raise = 0.07
 r = 0.04
+portion_down_payment = 0.25
 total_cost = 1000000
-downpayment = total_cost * 0.25
-
-annual_salary = float(input("Enter annual salary: "))
-
 current_savings = 0.0
 number_of_months = 36
-max_savings_rate = 10000
-low = 0
-high = max_savings_rate
-epsilon = 1
-guess = (high + low)/2.0
-steps_in_bisection_search = 0
+downpayment = total_cost * portion_down_payment
 
-while abs(guess - high) >= epsilon:
+starting_salary = float(input("Enter the starting salary: "))
+
+epsilon = 1
+number_of_guesses = 0
+low = 0
+high = 10000
+guess = (high + low)//2
+
+while abs(low - high) > epsilon:
+    annual_salary = starting_salary
+    monthly_salary = annual_salary / 12
+    current_savings = 0.0
     
+    for n in range(number_of_months):
+        current_savings += (monthly_salary * (guess/10000)) + (current_savings*(r/12))
+        if (n + 1) % 6 == 0:
+            annual_salary = annual_salary * (1 + semi_annual_raise)
+            monthly_salary = annual_salary / 12
     
-    guess = (high + low)/2.0
-    steps_in_bisection_search += 1
+    if abs(current_savings - downpayment) <= 100:
+        break
+    elif current_savings < downpayment:
+        low = guess
+    else:
+        high = guess
     
-    
-print("Number of months:", number_of_months)
+    guess = (high + low)//2
+    number_of_guesses += 1
+
+if abs(current_savings - downpayment) < 100:
+    print("Best savings rate:", guess/10000)
+    print("Steps in bisection search:", number_of_guesses)
+else:
+    print("It is not possible to pay the down payment in three years.")
